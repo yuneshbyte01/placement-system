25d39ec0ffba436d68e5e4cb435883703e1cb205
Yunesh Timsina <yuneshtimsina@gmail.com>
2025-08-19 22:38:17 +0545

    initialize placement-system project

---
+++ src/main/java/com/university/placementsystem/PlacementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.university.placementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/university/placementsystem/PlacementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.placementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
